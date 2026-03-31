5363aefb34daf1858001eb212713f7cbd3c20040
LoungeKing123 <jssddavis@gmail.com>
2026-03-31 08:49:41 -0400

    Update run.ps1

--- powershell/run.ps1
+++ powershell/run.ps1
@@ -1,4 +1,5 @@
 Import-Module $PWD/powershell/copy.psm1
 $config = Get-Content -Path "$PWD\config.json" | ConvertFrom-Json
 Copy-Folders -SourcePaths $config.sources -Destination $config.destination -Recurse:$config.recurse -Force:$config.force
+write-host "Copying completed."
 
